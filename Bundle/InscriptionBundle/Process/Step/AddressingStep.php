7b4390aeec3ebbeb3f3c071762bd1be3f0d712c2
zhang ning <ning.zhang@supinfo.com>
2013-12-11 21:59:37 +0100

    Merge register user and address together

--- Bundle/InscriptionBundle/Process/Step/AddressingStep.php
+++ Bundle/InscriptionBundle/Process/Step/AddressingStep.php
@@ -26,18 +26,42 @@ class AddressingStep extends ControllerStep
         $form = $this->getAddressingForm();
 
         
-        
-        
         if ($request->isMethod('POST') && $form->bind($request)->isValid()) {
-        	/*
-            $this->dispatchCheckoutEvent(SyliusCheckoutEvents::ADDRESSING_PRE_COMPLETE, $order);
+     	
+        	$userManager = $this->get('fos_user.user_manager');
+        	$dispatcher = $this->get('event_dispatcher');
+        	$user = $userManager->createUser();
+        	$user->setEnabled(true);
+        	
+        	$login  = $context->getStorage()->get('my_data')['email'];
+        	$pass   = $context->getStorage()->get('my_data')['plainPassword']['first'];
+        	$mobile = $context->getStorage()->get('my_data')['mobile'];
+        	$firstName = $context->getStorage()->get('my_data')['firstName'];
+        	$lastName  = $context->getStorage()->get('my_data')['lastName'];
+        	
+        	$user->setUsername($login);
+        	$user->setPlainPassword($pass);
+        	$user->setMobile($mobile);
+        	$user->setEmail($login);
+        	$user->setFirstName($firstName);
+        	$user->setLastName($lastName);
+        	$user->addAddress($form->getData());
+        	$user->setShippingAddress($form->getData());
+        	$user->setBillingAddress($form->getData());
+        	$userManager->updateUser($user);
+        	    	
+        	$event = new FormEvent($form, $request);
+	        
+	        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+	
+			$userManager->updateUser($user);
+	
+	        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, new Response()));
+        	
+        	
 
-            $this->getManager()->persist($order);
-            $this->getManager()->flush();
-
-            $this->dispatchCheckoutEvent(SyliusCheckoutEvents::ADDRESSING_COMPLETE, $order);
-*/
             return $this->complete();
+
             
         }
 
