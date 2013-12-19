ba63656a39356eb22f09750de57407c36fc07560
zhang ning <ning.zhang@supinfo.com>
2013-12-19 23:43:49 +0100

    add new service form.type for inscription_user

--- Bundle/InscriptionBundle/Form/Type/InscriptionType.php
+++ Bundle/InscriptionBundle/Form/Type/InscriptionType.php
@@ -2,24 +2,43 @@
 
 namespace Acme\Bundle\InscriptionBundle\Form\Type;
 
-use Sylius\Bundle\CoreBundle\Form\Type\UserType as BaseType;
-
+//use Sylius\Bundle\CoreBundle\Form\Type\UserType as BaseType;
+use FOS\UserBundle\Form\Type\ProfileFormType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
 
-class InscriptionType extends BaseType
+class InscriptionType extends ProfileFormType
 {
-	
+	/** @var string */
+    private $dataClass;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($dataClass)
+    {
+        $this->dataClass = $dataClass;
+    }
 	
 	/**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-		parent::buildForm($builder, $options);
+		
+    }
+    
+	/**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class'         => $this->dataClass
+        ));
     }
 	
 	/**
