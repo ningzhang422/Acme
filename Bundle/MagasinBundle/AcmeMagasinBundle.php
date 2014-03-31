03507a9bef326d66120e3707771823a29539f407
zhang ning <ning.zhang@supinfo.com>
2014-03-31 23:51:23 +0200

    add new bundle magasin to manage shop local

---
+++ Bundle/MagasinBundle/AcmeMagasinBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\Bundle\MagasinBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeMagasinBundle extends Bundle
+{
+}
---
+++ Bundle/MagasinBundle/Entity/Magasin.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Acme\Bundle\MagasinBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Magasin
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Acme\Bundle\MagasinBundle\Entity\MagasinRepository")
+ */
+class Magasin
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="latitude", type="string", length=255)
+     */
+    private $latitude;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="longitude", type="string", length=255)
+     */
+    private $longitude;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Magasin
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param string $latitude
+     * @return Magasin
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return string 
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param string $longitude
+     * @return Magasin
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return string 
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+}
