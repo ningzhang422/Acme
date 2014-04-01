0a8ce0f405c34a718558f6a60d1d2cdb8d09ae6c
zhang ning <ning.zhang@supinfo.com>
2014-04-02 00:07:17 +0200

    add magasin interface in backoffice

--- Bundle/MagasinBundle/DependencyInjection/AcmeMagasinExtension.php
+++ Bundle/MagasinBundle/DependencyInjection/AcmeMagasinExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acme\MagasinBundle\DependencyInjection;
+namespace Acme\Bundle\MagasinBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
--- Bundle/MagasinBundle/DependencyInjection/Configuration.php
+++ Bundle/MagasinBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acme\MagasinBundle\DependencyInjection;
+namespace Acme\Bundle\MagasinBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
