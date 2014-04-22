7cb6841afc21ab63caf0fbc8d205250a4ba86e76
zhang <ning.zhang@supinfo.com>
2014-04-22 23:48:01 +0200

    modified shipment add creneau_id to note creneau per shipment

---
+++ Bundle/PeriodBundle/Entity/Shipment.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+namespace Acme\Bundle\PeriodBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Bundle\CoreBundle\Model\Shipment as BaseShipment;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_shipment")
+ */
+class Shipment extends BaseShipment
+{
+    /**
+     * @ORM\Column(name="creneau_id", type="integer")
+   	 * @ORM\ManyToOne(targetEntity="Acme\Bundle\PeriodBundle\Entity\Creneau")
+   	 */
+    private $creneau;
+    
+	/**
+     * Set creneau
+     *
+     * @param \Acme\Bundle\PeriodBundle\Entity\Creneau $creneau
+     * @return Creneau
+     */
+    public function setCreneau(\Acme\Bundle\PeriodBundle\Entity\Creneau $creneau=null)
+    {
+        $this->creneau = $creneau;
+
+        return $this;
+    }
+
+    /**
+     * Get creneau
+     *
+     * @return \Acme\Bundle\PeriodBundle\Entity\Creneau
+     */
+    public function getCreneau()
+    {
+        return $this->creneau;
+    }
+}
