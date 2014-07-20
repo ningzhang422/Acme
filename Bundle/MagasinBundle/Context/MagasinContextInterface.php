f8c1147851214b1b846ff257479588a95e81b60b
zhang <ning.zhang@supinfo.com>
2014-07-20 23:21:02 +0200

    magasin context, make magasin_id as a session variable

---
+++ Bundle/MagasinBundle/Context/MagasinContextInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Acme\Bundle\MagasinBundle\Context;
+
+
+/**
+ * Interface to be implemented by the service providing the shop used
+ * magasin.
+ *
+ * @author Ning ZHANG
+ */
+interface MagasinContextInterface
+{
+    /**
+     * Get the default locale.
+     *
+     * @return LocaleInterface
+     */
+    public function getDefaultMagasin();
+
+    /**
+     * Get the currently active locale.
+     *
+     * @return LocaleInterface
+     */
+    public function getMagasin();
+
+    /**
+     * Set the currently active locale.
+     *
+     * @param LocaleInterface $locale
+     */
+    public function setMagasin($magasin);
+}
